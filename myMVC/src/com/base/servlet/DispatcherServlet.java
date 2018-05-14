3ea1e46b22477a34f5862feb0e2f4bc1fcec3bb9
haohj <haohj@outlook.com>
2018-05-14 11:22:34 +0800

    反射出类和方法

--- myMVC/src/com/base/servlet/DispatcherServlet.java
+++ myMVC/src/com/base/servlet/DispatcherServlet.java
@@ -1,14 +1,22 @@
 package com.base.servlet;
 
-import java.awt.peer.ChoicePeer;
+import com.base.annotation.Controller;
+import com.base.annotation.RequestMapping;
+import com.base.annotation.Service;
+
 import java.io.File;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DispatcherServlet {
 
     private List<String> packageNames = new ArrayList<String>();
+    private Map<String, Object> classMap = new HashMap<String, Object>();
+    private Map<String,Object> methodMap = new HashMap<String, Object>();
 
     public String replacePackageName(String packageName) {
         return packageName.replaceAll("\\.", "/");
@@ -62,17 +70,83 @@ public class DispatcherServlet {
 
         for (String pkn : packageNames) {
             //根据类的全限定名通过反射获取指定类的class对象
-
+            Class<?> object = Class.forName(pkn.replace(".class", "").trim());
             //判断该类是否有controller注解
-            //实例化该class的类对象
-            //获取controller注解的对象
-            //获取该注解的参数的值
-            //判断注解的值是否为空
-            //获取该类的类名首字母小写作为值。
-
-
-            //判断该对象是否有service注解
-            //将类的名称和类的实例放到map对象中
+            if (object.isAnnotationPresent(Controller.class)) {
+                //实例化该class的类对象
+                Object instance = object.newInstance();
+                //获取controller注解的对象
+                Controller controller = object.getAnnotation(Controller.class);
+                //获取该注解的参数的值
+                String key = controller.value();
+                //判断注解的值是否为空
+                if (key.equals("")) {
+                    //获取该类的类名首字母小写作为值。
+                    key = object.getSimpleName().substring(0, 1).toLowerCase() + object.getSimpleName().substring(1);
+                }
+                //将类的名称和类的实例放到map对象中
+                classMap.put(key, instance);
+            } else if (object.isAnnotationPresent(Service.class)) {
+                Object instance = object.newInstance();
+                Service service = object.getAnnotation(Service.class);
+                String key = service.value();
+                if (key.equals("")) {
+                    key = object.getSimpleName().substring(0, 1).toLowerCase() + object.getSimpleName().substring(1);
+                }
+                classMap.put(key, instance);
+            } else {
+                continue;
+            }
         }
+    }
+
+    public void handlerMap() {
+        //判断类的集合是否为空
+        if (classMap.size() <= 0) {
+            return;
+        }
+
+        for (Map.Entry<String, Object> entry : classMap.entrySet()) {
+            //获取controller注解对象
+            if (entry.getValue().getClass().isAnnotationPresent(Controller.class)) {
+                Controller controller = entry.getValue().getClass().getAnnotation(Controller.class);
+                //获取controller注解的值
+                String value = controller.value();
+                //判断注解的值是否为空
+                if (value.equals("")) {
+                    //获取该类的类名首字母小写作为值
+                    value = entry.getValue().getClass().getSimpleName().substring(0, 1).toLowerCase() + entry.getValue().getClass().getSimpleName().substring(1);
+                }
+                //反射获取该类下面的所有方法
+                Method[] methods = entry.getValue().getClass().getMethods();
+                for (Method method : methods) {
+                    //判断该方法是否有RequestMapping注解
+                    if (method.isAnnotationPresent(RequestMapping.class)) {
+                        //获取RequestMapping对象
+                        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+                        //获取注解的值
+                        String rv = requestMapping.value();
+                        //判断注解的值是否为空
+                        if (rv.equals("")) {
+                            //获取该类的类名首字母小写作为值
+                            rv = method.getClass().getSimpleName().substring(0, 1).toLowerCase() + method.getClass().getSimpleName().substring(1);
+                        }
+                        //将controller的名称和方法的名称保存到集合中
+                        methodMap.put("/" + value + rv, method);
+                    }else {
+                        continue;
+                    }
+                }
+            }else {
+                continue;
+            }
+        }
+    }
+
+    public void ioc(){
+        if (classMap.size() <= 0) {
+            return;
+        }
+
     }
 }
